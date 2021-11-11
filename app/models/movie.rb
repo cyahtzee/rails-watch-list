88894a515e028ba973359e323a6dff3d0246f61c
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-11 12:28:20 +0900

    Added models

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+  validates :overview, :title, presence: true
+  validates :title, uniqueness: true
+end
