06fa03c23611b3c90228e37f54eebe70ddaa592b
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-11 14:24:37 +0900

    Added a navbar and banner

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to :lists_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'lists#index'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create] do
+    end
+  end
+  resources :bookmarks, only: [:delete]
 end
